d69b89d08a9b4ee49bcb9500d3121d351bf72b20
Ksenon07147 <41545450+Ksenon07147@users.noreply.github.com>
2020-06-20 23:49:52 +0800

    initializing

---
+++ Home Element/checkLogin.js
@@ -0,0 +1,48 @@
+﻿/*This is a javascript file to validate the username and password if correct jump to logged homeppage*/
+
+
+function check(){
+	var username = document.getElementById("user").value;
+	var password = document.getElementById("pass").value;
+
+
+	if (user!="lim123"&&password!="testing"){
+		alert("Username or password wrong, please enter again");
+		return false;
+		document.getElementById("user").focus();
+		
+	}
+	else{
+		alert("Welcome , Ms Lim Lay Kheng");
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
